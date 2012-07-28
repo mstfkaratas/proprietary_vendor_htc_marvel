0926eee70306952b11a901000f02a504605faffe
Simon Sickle <simonsimons34@gmail.com>
2012-07-27 22:53:47 -0400

    cleaning up a bit

---
+++ marvel/marvel-vendor-blobs.mk
@@ -0,0 +1,25 @@
+PRODUCT_COPY_FILES += \
+    vendor/htc/marvel/proprietary/libcamera.so:obj/lib/libcamera.so
+
+PRODUCT_COPY_FILES += \
+    vendor/htc/marvel/proprietary/akmd:/system/bin/akmd \
+    vendor/htc/marvel/proprietary/AudioFilter.csv:/system/etc/AudioFilter.csv \
+    vendor/htc/marvel/proprietary/AudioPara4.csv:/system/etc/AudioPara4.csv \
+    vendor/htc/marvel/proprietary/AudioPara4_WB.csv:/system/etc/AudioPara4_WB.csv \
+    vendor/htc/marvel/proprietary/AudioPreProcess.csv:/system/etc/AudioPreProcess.csv \
+    vendor/htc/marvel/proprietary/WPDB.zip:/system/etc/WPDB.zip \
+    vendor/htc/marvel/proprietary/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
+    vendor/htc/marvel/proprietary/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
+    vendor/htc/marvel/proprietary/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
+    vendor/htc/marvel/proprietary/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/htc/marvel/proprietary/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
+    vendor/htc/marvel/proprietary/libmm-adspsvc.so:/system/lib/libmm-adspsvc.so \
+    vendor/htc/marvel/proprietary/libOmxH264Dec.so:/system/lib/libOmxH264Dec.so \
+    vendor/htc/marvel/proprietary/libOmxMpeg4Dec.so:/system/lib/libOmxMpeg4Dec.so \
+    vendor/htc/marvel/proprietary/libOmxVidEnc.so:/system/lib/libOmxVidEnc.so \
+    vendor/htc/marvel/proprietary/libcamera.so:/system/lib/libcamera.so \
+    vendor/htc/marvel/proprietary/libgsl.so:/system/lib/libgsl.so \
+    vendor/htc/marvel/proprietary/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
+    vendor/htc/marvel/proprietary/libaudioeq.so:/system/lib/libaudioeq.so \
+    vendor/htc/marvel/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \
+    vendor/htc/marvel/proprietary/liboemcamera.so:/system/lib/liboemcamera.so
